203b6f52449ec33e635e188e39f8aec59617bb1d
Tony <tonytnportela@outlook.com>
2022-11-21 22:56:05 -0300

    one more day

--- src/router.ts
+++ src/router.ts
@@ -1,51 +1,47 @@
 import { Router } from 'express';
+import { listCategories } from './app/useCases/categories/listCategories';
+import { createCategory } from './app/useCases/categories/createCategory';
+
 
 export const router = Router();
 
-
 // list categoriers
-router.get('/categories', (req, res) => {
-  res.send('ok!');
-}) ;
-
+router.get('/categories', listCategories);
 
 // create categoriy
-router.post('/categories', (req, res) => {
-  res.send('ok!');
-}) ;
+router.post('/categories', createCategory);
 
 // list products
 router.get('/products', (req, res) => {
   res.send('ok!');
-}) ;
+});
 
 // create product
 router.post('/products', (req, res) => {
   res.send('ok!');
-}) ;
+});
 
 // get product by category
 router.get('/categories/:categoryId/products', (req, res) => {
   res.send('ok!');
-}) ;
+});
 
 // list orders
 router.get('/orders', (req, res) => {
   res.send('ok!');
-}) ;
-
+});
 
 // create order
 router.post('/orders', (req, res) => {
   res.send('ok!');
-}) ;
+});
 
 // change order status
 router.patch('/orders/:orderID', (req, res) => {
   res.send('ok!');
-}) ;
+});
 
 // delete/cancel order
 router.delete('/orders/:orderID', (req, res) => {
   res.send('ok!');
-}) ;
+});
