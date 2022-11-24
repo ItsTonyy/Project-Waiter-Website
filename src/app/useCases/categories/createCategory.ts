11a27f42cb507ea3e3db3aa60f9e8db76b01b0d0
Tony <tonytnportela@outlook.com>
2022-11-23 23:36:20 -0300

    updates from today

--- src/app/useCases/categories/createCategory.ts
+++ src/app/useCases/categories/createCategory.ts
@@ -3,9 +3,26 @@ import { Request, Response } from 'express';
 import { Category } from '../../models/Category';
 
 export async function createCategory(req: Request, res: Response) {
-  const { icon, name } = req.body;
-  
-  const category = await Category.create({ icon, name });
+  try {
+    const { icon, name } = req.body;
 
-  res.json(category);
+    if(!name) {
+      return res.status(400).json({
+        error: 'Name is required',
+      });
+    }
+
+    if(!icon) {
+      return res.status(400).json({
+        error: 'Icon is required',
+      });
+    }
+  
+    const category = await Category.create({ icon, name });
+
+    res.status(201).json(category);
+  } catch (error) {
+    res.sendStatus(500);
+    console.log('error');
+  }
 }
